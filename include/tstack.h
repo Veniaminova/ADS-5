d0ecc8a5234978c25a48922ee6b884c4faa47129
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-04-06 11:19:42 +0300

    tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,33 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+    T* _stack;
+    int _max_size;
+    int cur;
+public:
+    TStack() {
+        _stack = new T[size];
+        _max_size = size;
+        cur = -1;
+    }
+    ~TStack() {
+        delete[] _stack;
+    }
+    void push(T elem) {
+        cur++;
+        if (cur >= _max_size) {
+            return;
+        }
+        _stack[cur] = elem;
+     }
+    T top() {
+        return _stack[cur];
+    }
+    void pop() {
+        cur--;
+    }
+    bool empty() {
+        return cur == -1;
+    }
 };
-
 #endif  // INCLUDE_TSTACK_H_

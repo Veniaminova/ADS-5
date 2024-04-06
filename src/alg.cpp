3606afb92d964d13a15b4a46ae205487eb3de7d3
Анастасия <98629650+Veniaminova@users.noreply.github.com>
2024-04-06 17:19:31 +0300

    alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,76 +3,88 @@
 #include <map>
 #include "tstack.h"
 
-int p_o(char c) {
-    if (c == '+' || c == '-') return 1;
-    if (c == '*' || c == '/') return 2;
-    return 0;
+int prioryty_oper(char x) {
+    switch (x) {
+    case '(':
+        return 0;
+    case ')':
+        return 1;
+    case '+': case '-':
+        return 2;
+    case '*': case '/':
+        return 3;
+    default:
+        return -1;
+    }
 }
-
 std::string infx2pstfx(std::string inf) {
-    std::string res = "";
-    TStack <char, 100> st;
-    for (int i = 0; i < inf.size(); ++i) {
-        if (isdigit(inf[i])) {
-            while (isdigit(inf[i]) && i != inf.size()) {
-                res += inf[i];
-                ++i;
+    std::string rez, rez1;
+    TStack <char, 100> stack1;
+    for (auto& x : inf) {
+        int p = prioryty_oper(x);
+        if (p == -1) {
+            rez = rez + x + ' ';
+        } else {
+            char elem = stack1.get();
+            if (p == 0 || prioryty_oper(elem) < p || stack1.isEmpty()) {
+                stack1.push(x);
+            } else {
+                if (x == ')') {
+                    while (prioryty_oper(elem) >= p) {
+                        rez = rez + elem + ' ';
+                        stack1.pop();
+                        elem = stack1.get();
+                    }
+                    stack1.pop();
+                } else {
+                    while (prioryty_oper(elem) >= p) {
+                        rez = rez + elem + ' ';
+                        stack1.pop();
+                        elem = stack1.get();
+                    }
+                    stack1.push(x);
+                }
             }
-            --i;
-            res += " ";
-            continue;
-        } else if (inf[i] == '(') {
-            st.push('(');
-        } else if (inf[i] == ')') {
-            while (st.top() != '(') {
-                res += st.top();
-                res += " ";
-                st.pop();
-            }
-            st.pop();
-        } else if (p_o(inf[i]) != -1) {
-            int cur_priority = p_o(inf[i]);
-            while (!st.empty() && cur_priority <= p_o(st.top())) {
-                res += st.top();
-                res += " ";
-                st.pop();
-            }
-            st.push(inf[i]);
         }
     }
-    while (!st.empty()) {
-        res += st.top();
-        res += " ";
-        st.pop();
+    while (!stack1.isEmpty()) {
+        rez = rez + stack1.get() + ' ';
+        stack1.pop();
     }
-    return res;
+    for (int i = 0; i < rez.size() - 1; i++)
+        rez1 += rez[i];
+    return rez1;
 }
 
 int eval(std::string post) {
-    int a = 0;
-    int b = 0;
-    bool flag_first = true;
+    TStack<int, 100> stack1;
+    std::string rez = "";
     for (int i = 0; i < post.size(); ++i) {
-        if (isspace(post[i])) continue;
-        if (isdigit(post[i])) {
-            int t = 0;
-            while (isdigit(post[i]) && i != post.size()) {
-                t *= 10;
-                t += post[i] - '0';
-                ++i;
-            }
-            if (flag_first) {
-                a = t;
-                flag_first = false;
+        if (prioryty_oper(post[i]) == -1) {
+            if (post[i] == ' ') {
+                continue;
+            } else if (isdigit(post[i + 1])) {
+                rez += post[i];
+                    continue;
             } else {
-                b = t;
+                rez += post[i];
+                stack1.push(atoi(rez.c_str()));
+                rez = "";
             }
-        } else {
-            if (post[i] == '+') a = a + b;
-            if (post[i] == '-') a = a - b;
-            if (post[i] == '*') a = a * b;
-            if (post[i] == '/') a = a / b;
+        else {
+            int b = stack1.get();
+            stack1.pop();
+            int a = stack1.get();
+            stack1.pop();            
+            if (post[i] == '+')
+                stack1.push(a + b);
+            if (post[i] == '-')
+                stack1.push(a - b);
+            if (post[i] == '*')
+                stack1.push(a * b);
+            if (post[i] == '/')
+                stack1.push(a / b);
         }
     }
-    return a;
+    return stack1.get();
 }
